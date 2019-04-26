d75169fc8a5bd25eb5446cef3be6ad995dfa48af
Richard Pena <penarichard53@gmail.com>
2019-04-26 12:56:46 -0700

    able to server fake database details/:crop

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express =  require('express');
 const app = express();
 
+const fakeDatabase = 
+{
+    'corn': {soil: 'dark', color: 'yellow'},
+    'grapes': {soil: 'light', color: 'green'},
+    'apples': {soil: 'medium', color: 'red'}
+}
 app.get('/', (req,res) => { // home page
     res.sendFile(__dirname + '/' + 'index.html')
 })
@@ -10,6 +16,15 @@ app.get('/home', (req,res) => {
     res.sendFile(__dirname + '/' + 'home.html')
 })
 
+app.get('/details/:cropName', (req,res) => {  
+    const cName = req.params.cropName  
+    const val = fakeDatabase[cName]
+    if (val) {
+        res.send(val)
+    } else {
+        res.send({})
+    }
+})
 
 app.listen(3000, () => { // set server to listen on port 3000
     console.log('Server started at http://localhost:3000/');
