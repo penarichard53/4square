ce363784b15a387f7b3aa3721e4cf23059b15e29
Richard Pena <penarichard53@gmail.com>
2019-04-27 09:13:13 -0700

    changed api call to ajax request

--- server.js
+++ server.js
@@ -13,40 +13,6 @@ app.get('/', (req,res) => { // home page
     res.sendFile(__dirname + '/' + 'index.html')
 })
 
-app.get('/weather/:zipcode', (req,res) => {
-        
-    // OpenWeatherMap api
-    const API_KEY = '&APPID=4ff9698a7bda3347679b3f775e88ef89'
-    const API_URL = 'https://api.openweathermap.org/data/2.5/forecast?zip='
-    const zip = req.params.zipcode
-    const countryCode = ',us'
-    const url = API_URL + zip + countryCode + API_KEY
-    var temp = {}
-    var description = {}
-    var timestamp = {}
-
-
-
-    
-    // returns 5 day weather data
-    api_helper.make_API_call(url).then(response => {
-        const data = response
-        for(var i = 0; i < data.list.length; i++) {
-            console.log(data.list[i].dt_txt) // gets timestamp
-            timestamp = data.list[i].dt_txt
-            description = data.list[i].weather[0].description
-            temp = data.list[i].main.temp - 273.15
-            temp = Math.floor(temp * (9/5) + 32)
-            console.log(description);            
-            console.log(temp);           
-            console.log('\n\n\n\n\n\n\n\n\n')   
-        }
-        res.json(data)
-    })
-    .catch(error => {
-        console.log("error found", error)
-    })    
-})
 
 app.get('/home', (req,res) => {
     res.sendFile(__dirname + '/' + 'home.html')
